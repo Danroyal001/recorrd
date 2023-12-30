64d4d98f550516f947035dae431dbadbb5470400
dorafina <thessyzilla@gmail.com>
2023-12-30 14:33:11 +0100

    splashScreen Completed

--- lib/presentation/splash_screen/splash_screen.dart
+++ lib/presentation/splash_screen/splash_screen.dart
@@ -2,23 +2,44 @@ import 'controller/splash_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:recorrd/core/app_export.dart';
 
-class SplashScreen extends GetWidget<SplashController> {
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    navigateToNextScreen();
+  }
+
+  void navigateToNextScreen() async {
+    await Future.delayed(Duration(seconds: 3));
+    Get.offNamed('/OnboardingGuideScreen');
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-            backgroundColor: ColorConstant.gray50,
-            body: Container(
-                width: double.maxFinite,
-                padding: getPadding(left: 33, right: 33),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      CustomImageView(
-                          svgPath: ImageConstant.imgVectorBlueA70034x360,
-                          height: getVerticalSize(34),
-                          width: getHorizontalSize(360),
-                          margin: getMargin(bottom: 5))
-                    ]))));
+      child: Scaffold(
+        backgroundColor: ColorConstant.primaryColor,
+        body: Container(
+          width: double.maxFinite,
+          padding: getPadding(left: 33, right: 33),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/recorrd-pro.png'),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
